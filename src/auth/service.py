9bce42c4472a89353e2fc43cd54f3dc55328b5a7
Abasifreke Isaac <79318104+Abas-dev@users.noreply.github.com>
2024-09-18 06:10:53 +0100

    added route to create a user

--- src/auth/service.py
+++ src/auth/service.py
@@ -3,10 +3,11 @@ from sqlalchemy.orm import Session
 
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
-from jose import jwt, JWTError
+from jose import jwt, JWTError # type: ignore
 from datetime import timedelta, datetime
 
 from src.auth.models import User 
+from src.auth.schemas import UserCreate
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2bearer = OAuth2PasswordBearer(tokenUrl="v1/auth/token")
@@ -42,3 +43,23 @@ async def get_current_user(db:Session, token: str = Depends(oauth2bearer)):
     
 async def get_user_from_user_id(db:Session,user_id:int):
     return db.query(User).filter(User.id == user_id).first()
+
+async def create_user_srv(db:Session, user:UserCreate):
+    db_user = User(
+        email = user.email.lower().strip(),
+        username = user.username.lower().strip(),
+        hashed_password = bcrypt_context.hash(user.password),
+        dob = user.dob or None,
+        gender = user.gender or None,
+        bio = user.bio or None,
+        location = user.location or None, 
+        profile_pic = user.profile_pics or None, 
+        name = user.name or None 
+    )
+    db.add(db_user)
+    db.commit()
+
+    return db_user
+
+async def authenticate(db: Session, username: str, password: str):
+    ...
--- src/auth/views.py
+++ src/auth/views.py
@@ -5,4 +5,28 @@ from datetime import datetime
 
 from src.auth.schemas import UserCreate, UserUpdate, User as UserSchema 
 from src.database import get_db 
+from src.auth.service import create_access_token, existing_user, get_current_user,get_user_from_user_id, create_user_srv
 
+router = APIRouter(prefix='/auth', tags=['auth'])
+
+@router.post("/signup",status_code=status.HTTP_201_CREATED)
+async def create_user(user:UserCreate, db:Session = Depends(get_db)):
+    # check for existing user 
+    db_user = await existing_user(db, user.username, user.email)
+    if db_user:
+        raise HTTPException(
+            status_code= status.HTTP_409_CONFLICT, 
+            detail="username or email already in use", 
+        )
+    db_user = create_user_srv(db, user)
+    access_token = await create_access_token(user.username, db_user.id)
+
+    return {
+        "access_token": access_token, 
+        "token_type": "bearer",
+        "username": user.username
+    }
+
+@router.post("/token", status_code=status.HTTP_201_CREATED)
+async def login(form_data: OAuth2PasswordBearer = Depends(), db:Session = Depends(get_db)):
+    db_user =   ...
